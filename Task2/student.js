8e46705e4ab9af876fb56e37e4ba41dc63f29ed1
Rakshanda wankhade <rakshandawankhade95@gmail.com>
2022-10-20 17:18:27 +0530

    student object

--- Task2/student.js
+++ Task2/student.js
@@ -1,8 +1,24 @@
-const student = {
-  name: "Max",
-  age: 20,
-  greet() {
-    console.log(" Hi, I am " + this.name);
-  },
-};
-student.greet();
+class student {
+  constructor(name, age, marks) {
+    this.name = name;
+    this.age = age;
+    this.marks = marks;
+  }
+  setPlacementAge(minPlacementAge) {
+    console.log(this);
+    return (minMarks) => {
+      //console.log("inside egligibleForCurrentCompany", this);
+      if (this.marks > minMarks && this.age > minPlacementAge) {
+        console.log(this.name + " is ready for placements");
+      } else {
+        console.log(this.name + " is not ready for placements");
+      }
+    };
+  }
+}
+
+const Rakshanda = new student("Rakshanda", 27, 60);
+const Chetan = new student("Chetan", 28, 35);
+
+Rakshanda.setPlacementAge(18)(40);
+Chetan.setPlacementAge(28)(35);
