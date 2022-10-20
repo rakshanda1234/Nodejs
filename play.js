22897942b5251c297bb54234b1f6f473b32919a5
Rakshanda wankhade <rakshandawankhade95@gmail.com>
2022-10-20 14:45:14 +0530

    product of two numbers

---
+++ play.js
@@ -0,0 +1,29 @@
+const name = "Max";
+let age = 29;
+const hasHobbies = true;
+
+// name = "Maximilian";
+age = 30;
+
+const summarizeUser = (userName, userAge, userHasHobby) => {
+  this;
+  return (
+    " Name is " +
+    userName +
+    ", age is " +
+    userAge +
+    " and the user has hobbies: " +
+    userHasHobby
+  );
+};
+
+//const add = (a, b) => a + b;
+//const addOne = (a) => a + 1;
+//const addRandom = () => 1 + 2;
+const product = () => 2 * 2;
+
+//console.log(add(1, 2));
+//console.log(addOne(1));
+//console.log(addRandom());
+console.log(product());
+console.log(summarizeUser(name, age, hasHobbies));
