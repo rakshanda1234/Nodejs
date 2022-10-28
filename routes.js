6f8e28754925dd2dc8b5f114aee688bc3a79a41f
Rakshanda wankhade <rakshandawankhade95@gmail.com>
2022-10-28 17:33:17 +0530

    file uploaded

--- routes.js
+++ routes.js
@@ -44,8 +44,8 @@ const requestHandler = (req, res) => {
 //    someText :'Some hard coded text'
 // };
 
-// module.exports.handler = requestHandler;
-// module.exports.someText = "Some hard coded Text";
+module.exports.handler = requestHandler;
+module.exports.someText = "Some hard coded Text";
 
-exports.handler = requestHandler;
-exports.someText = "Some hard coded Text";
+// exports.handler = requestHandler;
+// exports.someText = "Some hard coded Text";
