d2a48b8e246640bb0485e88afe960eba32930369
Rakshanda wankhade <rakshandawankhade95@gmail.com>
2022-11-02 13:35:57 +0530

    /admin routes as a separete filter

--- write_form.js
+++ write_form.js
@@ -8,8 +8,8 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
-app.use(adminRoutes);
 
 app.use((req, res, next) => {
   res.status(404).send("<h1>Page not Found</h1>");
