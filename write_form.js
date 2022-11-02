735368e3ee0522610560fd513373cbda8e8dd7e1
Rakshanda wankhade <rakshandawankhade95@gmail.com>
2022-11-02 12:36:51 +0530

    file uploaded

--- write_form.js
+++ write_form.js
@@ -5,19 +5,13 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use("/", (req, res, next) => {
-//   console.log("This always runs!");
-//   next();
-// });
-
 app.use("/add-product", (req, res, next) => {
   res.send(
     '<form action="/product" method = "POST"><input type="text" name="title"><button type = "submit">Add Product</button></form>'
   );
-  //next(); //allows the request to continues to the next middleware in line
 });
 
-app.use("/product", (req, res, next) => {
+app.post("/product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
@@ -27,7 +21,6 @@ app.use("/", (req, res, next) => {
 });
 
 // const server = http.createServer(app);
-
 // server.listen(3000);
 
 app.listen(3000); //shortcut
