95f35a8fe2fa1ea003cd734a98bb9c08b675368d
Rakshanda wankhade <rakshandawankhade95@gmail.com>
2022-11-02 13:22:44 +0530

    Task4 Routers deliverable 1-clean up the code. deliverable 2 - 404 error page not found

--- write_form.js
+++ write_form.js
@@ -3,24 +3,16 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method = "POST"><input type="text" name="title"><input type="number" name="size"><button type = "submit">Add Product</button></form>'
-  );
-});
+app.use(shopRoutes);
+app.use(adminRoutes);
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not Found</h1>");
 });
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>hello from Express!</h1>");
-});
-
-// const server = http.createServer(app);
-// server.listen(3000);
 
 app.listen(3000); //shortcut
