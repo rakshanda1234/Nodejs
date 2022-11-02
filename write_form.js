757a2d09125b9c379d8d02c67c325d0f6428e7f7
Rakshanda wankhade <rakshandawankhade95@gmail.com>
2022-11-02 12:21:51 +0530

    task3 deliverable1

--- write_form.js
+++ write_form.js
@@ -1,16 +1,28 @@
 const express = require("express");
-// const routes = require("./routes");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log(" In the middleware!");
-  next(); //allows the request to continues to the next middleware in line
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use("/", (req, res, next) => {
+//   console.log("This always runs!");
+//   next();
+// });
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method = "POST"><input type="text" name="title"><button type = "submit">Add Product</button></form>'
+  );
+  //next(); //allows the request to continues to the next middleware in line
 });
 
-app.use((req, res, next) => {
-  console.log(" In another middleware!");
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 
+app.use("/", (req, res, next) => {
   res.send("<h1>hello from Express!</h1>");
 });
 
